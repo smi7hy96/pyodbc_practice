61a82af11badb42b12d57990d209e4f39f47f517
Ryan Smith <smithy.ryan96@hotmail.co.uk>
2020-06-10 11:41:24 +0100

    added universal function for querying

---
+++ universal_query_function.py
@@ -0,0 +1,16 @@
+from db_connect import *
+
+def return_query_results(conn, query):
+    with conn:
+        cursor = conn.cursor()
+        query = cursor.execute(query)
+        while True:
+            row = query.fetchone()
+            if row is None:
+                break
+            print(row)
+
+
+connection = db_connect_me()
+query = "SELECT * FROM Orders o WHERE o.ShipCity IN ('Rio De Janeiro', 'Reims')"
+return_query_results(connection, query)
