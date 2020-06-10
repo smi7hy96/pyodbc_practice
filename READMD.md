0e4e28abb44949b4e95e1208bdf4697ac5341766
Ryan Smith <smithy.ryan96@hotmail.co.uk>
2020-06-10 12:26:46 +0100

    added oop init and establish. absrtaction started

--- READMD.md
+++ READMD.md
@@ -5,3 +5,28 @@ This class will allow our python to easily connect with our DB.
 To do so, we will use a package called PYODBC.
 
 This package abstracts our connections to the DB.
+
+# PYODBC and OOP
+
+We are looking into the PYODBC package.
+
+And we are going to use the package to make a OOP module, that abstracts our interaction with the NW Database.
+
+And then further abstract the interaction with specific Tables.
+
+Finally, where appropriate we will use the CRUD design to build methods. 
+
+
+## CRUD
+
+### CREATE -
+
+### READ -
+
+#### - READ ONE
+
+#### - READ ALL
+
+### UPDATE -
+
+### DELETE -
---
+++ db_connection_oop.py
@@ -0,0 +1,35 @@
+import pyodbc
+
+class MSDBConnection:
+    def __init__(self, database="Northwind", server='databases2.spartaglobal.academy', username='SA', password='Passw0rd2018'):        # Establish connection with any db in MSsql
+        self.server = server
+        self.database = database
+        self.username = username
+        self.password = password
+        # Making ocnnection & Cursor
+        self.conn = self._establish_connection()
+        self.cursor = self.conn.cursor()
+
+    def _establish_connection(self):
+        conn = 'DRIVER={SQL Server};SERVER=' + self.server + ';DATABASE=' + self.database + ';UID=' + self.username + ';PWD=' + self.password
+        return pyodbc.connect(conn)
+
+    # open to sql injections - use a separate method to validate string first
+    def __sql_query_execute(self, sql_string):
+        return self.cursor.execute(sql_string)
+
+    def sql_query(self, sql_string):
+        # Filter and Validate First Here
+        return self.__sql_query_execute(sql_string)
+
+
+
+# nwind = MSDBConnection()
+# results = nwind.sql_query("SELECT * FROM PRODUCTS")
+#
+# while True:
+#     row = results.fetchone()
+#     if row is None:
+#         break
+#     print(row.ProductName)
+
